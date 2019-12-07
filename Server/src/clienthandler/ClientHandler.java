31dd4549b525264a29a2b1e2b61e448e3b198d59
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-12-07 21:50:00 +0100

    resena rekurzija

--- Server/src/clienthandler/ClientHandler.java
+++ Server/src/clienthandler/ClientHandler.java
@@ -141,7 +141,7 @@ public class ClientHandler extends Thread {
 	}
 	public void registracija() {
 		boolean validan = false;
-		int opcijaint=0;
+		//int opcijaint=0;
 		try {
 			PrintWriter upisUfajl = new PrintWriter(new FileWriter("korisnici.txt",true));
 			FileReader fr= new FileReader("korisnici.txt");
@@ -202,6 +202,53 @@ public class ClientHandler extends Thread {
 		
 		
 	}
+	
+	public void recursivePrint(File[] arr,int index,int level) throws IOException  
+    { 
+		clientOutput = new PrintStream(socketZaKomunikaciju.getOutputStream());
+        // terminate condition 
+        if(index == arr.length) 
+            return; 
+          
+        // tabs for internal levels 
+        for (int i = 0; i < level; i++) 
+        	clientOutput.print("\t"); 
+          
+        // for files 
+        if(arr[index].isFile()) 
+            clientOutput.println(arr[index].getName()); 
+          
+        // for sub-directories 
+        else if(arr[index].isDirectory()) 
+        { 
+        	clientOutput.println("[" + arr[index].getName() + "]"); 
+              
+            // recursion for sub-directories 
+            recursivePrint(arr[index].listFiles(), 0, level + 1); 
+        } 
+           
+        // recursion for main directory 
+        recursivePrint(arr,++index, level); 
+   } 
+	public File vratiFajlRekurzivno(File[] arr, int index, String nazivFajla) {
+		if(index==arr.length)
+			return null;
+		if(arr[index].isFile()) {
+			if(arr[index].getName().equals(nazivFajla)) {
+				return arr[index];
+				}
+		}else 
+			return vratiFajlRekurzivno(arr[index].listFiles(), 0, nazivFajla);
+		return vratiFajlRekurzivno(arr, ++index, nazivFajla);
+	}
+	public void listanje(String username) throws IOException {
+		clientOutput = new PrintStream(socketZaKomunikaciju.getOutputStream());
+		String putanja = new File("").getAbsolutePath();
+		putanja=putanja.concat("\\korisnici\\").concat(username);
+		File folder = new File(putanja);
+		File[] listaFoldera = folder.listFiles();
+		recursivePrint(listaFoldera, 0, 0);
+	}
 	public void listanjeDirektorijuma(String username) {
 		try {
 			clientOutput = new PrintStream(socketZaKomunikaciju.getOutputStream());
@@ -213,8 +260,14 @@ public class ClientHandler extends Thread {
 			for (File file : listaFoldera) {
 				if(file.isFile())
 					clientOutput.println("Fajl: " + file.getName());
-				else if(file.isDirectory())
+				else if(file.isDirectory()) {
 					clientOutput.println("Folder: " + file.getName());
+					//listanjeDirektorijuma(file.getName());
+//					String novaPutanja = new File("").getAbsolutePath();
+//					novaPutanja = putanja.concat(file.getName());
+//					File folder1 = new File(novaPutanja);
+//					for
+				}
 			}	
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -222,20 +275,55 @@ public class ClientHandler extends Thread {
 		}
 		
 	}
+	public File vratiFajl(String username,String nazivFajla) {
+		try {
+			clientOutput = new PrintStream(socketZaKomunikaciju.getOutputStream());
+			String putanja = new File("").getAbsolutePath();
+			putanja=putanja.concat("\\korisnici\\").concat(username);
+			File folder = new File(putanja);
+			File[] listaFoldera = folder.listFiles();
+			
+			for (File file : listaFoldera) {
+				if(file.isFile()) {
+					clientOutput.println(file.getName());
+				if(file.getName().equals(nazivFajla))
+					return file;
+				}
+			}	
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+		
+	}
 	
+	public void otvoriFajl(File file) throws IOException {
+		clientOutput = new PrintStream(socketZaKomunikaciju.getOutputStream());
+		if(file==null)
+			clientOutput.println("Odabrani fajl ne postoji");
+		else {
+			if(file.isDirectory())clientOutput.println("Direktorijum je");			//listanjeDirektorijuma(username);
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String tekst;
+			while((tekst =br.readLine())!=null) {
+				clientOutput.println(tekst);
+			}
+			br.close();
+		}
+	}
 	@Override
 	public void run() {
 		
 		try {
-			PrintWriter upisUfajl = new PrintWriter(new FileWriter("korisnici.txt",true));
-			FileReader fr= new FileReader("korisnici.txt");
-			BufferedReader citanjeIzFajla = new BufferedReader(fr); 
+	//		PrintWriter upisUfajl = new PrintWriter(new FileWriter("korisnici.txt",true));
+	//		FileReader fr= new FileReader("korisnici.txt");
+	//		BufferedReader citanjeIzFajla = new BufferedReader(fr); 
 			clientInput = new BufferedReader(new InputStreamReader(socketZaKomunikaciju.getInputStream()));
 			clientOutput = new PrintStream(socketZaKomunikaciju.getOutputStream());
 			
-			boolean validan = false;
+	//		boolean validan = false;
 			int opcijaint=0;
-
+			
 			opcijaint=loginMeni();
 			if(opcijaint==0) {
 				clientOutput.println("Potvrdi izlaz unosom ***quit");
@@ -244,12 +332,27 @@ public class ClientHandler extends Thread {
 				registracija();
 			}
 			else if(opcijaint==2) {
+				
 				String korisnik=login();
+				do {
 				int opcija2=meniKorisnika(korisnik);
-				if(opcija2==1)listanjeDirektorijuma(korisnik);
-			}
+				if(opcija2==1)listanje(korisnik);
+				else if(opcija2==2) {
+					clientOutput.println("Unesi naziv fajla koji zelis da se prikaze: ");
+					String naziv = clientInput.readLine();
+					String putanja = new File("").getAbsolutePath();
+					putanja=putanja.concat("\\korisnici\\").concat(username);
+					File folder = new File(putanja);
+					File[] listaFoldera = folder.listFiles();
+					File file = vratiFajlRekurzivno(listaFoldera, 0, naziv);
+					
+				//	File file= vratiFajl(korisnik, naziv);
+					otvoriFajl(file);
+					
+				};
 			
-			else {
+			}while(opcijaint!=0);
+			}else {
 				
 			}
 			clientOutput.println("Upisi ***quit za izlaz");
